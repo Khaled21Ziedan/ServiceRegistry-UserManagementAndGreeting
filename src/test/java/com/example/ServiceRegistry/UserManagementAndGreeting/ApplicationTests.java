835048bd1ff9c7037aeb8418edae9b16c61a3df0
kmsaleh <khaledmohammedziedan@gmail.com>
2023-02-09 18:03:54 +0200

    Initial commit

---
+++ src/test/java/com/example/ServiceRegistry/UserManagementAndGreeting/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServiceRegistry.UserManagementAndGreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
